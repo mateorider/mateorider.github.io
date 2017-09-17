11f170f07b266952b6d13693726d876ac3e78f05
Matt Rider <matt.rider@octanner.com>
2017-09-16 23:29:28 -0600

    static-layour: render all components

--- calculator/src/component/App.js
+++ calculator/src/component/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Display from './Display'
+import ButtonPanel from './ButtonPanel'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Welcome to App
-      </div<
+        <Display />
+        <ButtonPanel />
+      </div>
     );
   }
 }
